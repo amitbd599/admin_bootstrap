1f6bcd4b7091f7bd87513789d10c2aa5d1c8b0c0
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-15 19:56:57 +0600

    Area Chart add

--- src/components/ChartsApexInner.jsx
+++ src/components/ChartsApexInner.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ApexCharts from "react-apexcharts";
 const ChartsApexInner = () => {
-  const chartConfig = {
+  const LineChartConfig = {
     series: [
       {
         name: "Desktops",
@@ -43,6 +43,57 @@ const ChartsApexInner = () => {
       },
     },
   };
+
+  const AreachartOptions = {
+    series: [
+      {
+        name: "STOCK ABC",
+        data: [
+          8107.85, 8128.0, 8122.9, 8165.5, 8340.7, 8423.7, 8423.5, 8514.3,
+          8481.85, 8487.7, 8506.9, 8626.2, 8668.95, 8602.3, 8607.55, 8512.9,
+          8496.25, 8600.65, 8881.1, 9340.85,
+        ],
+      },
+    ],
+    option: {
+      chart: {
+        type: "area",
+        height: 350,
+        zoom: { enabled: false },
+      },
+      dataLabels: { enabled: false },
+      stroke: { curve: "straight" },
+      subtitle: {
+        text: "Price Movements",
+        align: "left",
+      },
+      labels: [
+        "13 Nov 2017",
+        "14 Nov 2017",
+        "15 Nov 2017",
+        "16 Nov 2017",
+        "17 Nov 2017",
+        "20 Nov 2017",
+        "21 Nov 2017",
+        "22 Nov 2017",
+        "23 Nov 2017",
+        "24 Nov 2017",
+        "27 Nov 2017",
+        "28 Nov 2017",
+        "29 Nov 2017",
+        "30 Nov 2017",
+        "01 Dec 2017",
+        "04 Dec 2017",
+        "05 Dec 2017",
+        "06 Dec 2017",
+        "07 Dec 2017",
+        "08 Dec 2017",
+      ],
+      xaxis: { type: "datetime" },
+      yaxis: { opposite: true },
+      legend: { horizontalAlign: "left" },
+    },
+  };
   return (
     <section className='section'>
       <div className='row'>
@@ -52,8 +103,8 @@ const ChartsApexInner = () => {
               <h5 className='card-title'>Line Chart</h5>
               {/* Line Chart */}
               <ApexCharts
-                options={chartConfig.options}
-                series={chartConfig.series}
+                options={LineChartConfig.options}
+                series={LineChartConfig.series}
                 type='line'
                 height={350}
               />
@@ -66,7 +117,12 @@ const ChartsApexInner = () => {
             <div className='card-body'>
               <h5 className='card-title'>Area Chart</h5>
               {/* Area Chart */}
-              <div id='areaChart' />
+              <ApexCharts
+                options={AreachartOptions.option}
+                series={AreachartOptions.series}
+                type='area'
+                height={350}
+              />
               {/* End Area Chart */}
             </div>
           </div>
