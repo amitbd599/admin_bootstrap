0ac3428592c93f1f4c599da37c6f129fa10d9baf
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-15 10:59:26 +0600

    reportsChart add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import MasterLayout from "../layout/MasterLayout";
 import Breadcrumb from "../components/Breadcrumb";
+import HomeInner from "../components/HomeInner";
 
 const HomePage = () => {
   return (
     <MasterLayout>
       {/* Breadcrumb */}
       <Breadcrumb title={"Home"} />
+
+      {/* HomeInner */}
+      <HomeInner />
     </MasterLayout>
   );
 };
