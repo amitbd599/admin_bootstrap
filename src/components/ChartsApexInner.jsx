9b9eaaa131414366d9171170a007fcd00a8b43c8
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-01-15 20:33:03 +0600

    Bubble Chart add

--- src/components/ChartsApexInner.jsx
+++ src/components/ChartsApexInner.jsx
@@ -294,6 +294,73 @@ const ChartsApexInner = () => {
       labels: ["Apples", "Oranges", "Bananas", "Berries"],
     },
   };
+
+  const generateData = (baseval, count, yrange) => {
+    let i = 0;
+    const series = [];
+    while (i < count) {
+      const x = Math.floor(Math.random() * (750 - 1 + 1)) + 1;
+      const y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+      const z = Math.floor(Math.random() * (75 - 15 + 1)) + 15;
+      series.push([x, y, z]);
+      baseval += 86400000;
+      i++;
+    }
+    return series;
+  };
+
+  const BubbleChartOptions = {
+    series: [
+      {
+        name: "Bubble1",
+        data: generateData(new Date("11 Feb 2017 GMT").getTime(), 20, {
+          min: 10,
+          max: 60,
+        }),
+      },
+      {
+        name: "Bubble2",
+        data: generateData(new Date("11 Feb 2017 GMT").getTime(), 20, {
+          min: 10,
+          max: 60,
+        }),
+      },
+      {
+        name: "Bubble3",
+        data: generateData(new Date("11 Feb 2017 GMT").getTime(), 20, {
+          min: 10,
+          max: 60,
+        }),
+      },
+      {
+        name: "Bubble4",
+        data: generateData(new Date("11 Feb 2017 GMT").getTime(), 20, {
+          min: 10,
+          max: 60,
+        }),
+      },
+    ],
+    options: {
+      chart: {
+        height: 333,
+        type: "bubble",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      fill: {
+        opacity: 0.8,
+      },
+      xaxis: {
+        tickAmount: 12,
+        type: "category",
+      },
+      yaxis: {
+        max: 70,
+      },
+    },
+  };
   return (
     <section className='section'>
       <div className='row'>
@@ -437,7 +504,12 @@ const ChartsApexInner = () => {
             <div className='card-body'>
               <h5 className='card-title'>Bubble Chart</h5>
               {/* Bubble Chart */}
-              <div id='bubbleChart' />
+              <ApexCharts
+                options={BubbleChartOptions.options}
+                series={BubbleChartOptions.series}
+                type='bubble'
+                height={333}
+              />
               {/* End Bubble Chart */}
             </div>
           </div>
